b0b445c6cd81fdbf340cc4b7623da630bf2e9603
gearless-joeeee <bayesianifeg1@gmail.com>
2024-05-28 23:45:02 +0800

    suppor declare a winner

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,20 +1,61 @@
 import { useState } from 'react'
 
+function getNextFill(squares){
+    const filledSquares = squares.filter(square => (square === 'X' || square === 'O'))
+    const fillNumber = filledSquares.length
+    const nextFill = fillNumber % 2 === 0 ? 'X' : 'O'
+    return nextFill
+}
+
+function calculateBoardState(squares){
+  const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+    for(const winCondition of winConditions){
+      const [a, b, c] = winCondition
+      if(
+        !(squares[a] === null) 
+        && (squares[b] === squares[a])
+        && (squares[c] === squares[a])
+      ){
+        return `${squares[a]} won`
+      }
+    }
+
+
+  const squaresIsFull = squares.filter(square => (square === 'O' || square === 'X')).length === 9
+
+    if(squaresIsFull){
+      return 'Nobody won'
+    } else {
+      const nextFill = getNextFill(squares)
+      return `Next player is: ${nextFill}`
+    }
+}
+
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
+
+  const status = calculateBoardState(squares)
 
   const clickHandler = (index) => {
     const curentSquare = squares[index]
     if(!(curentSquare === null)) return 
-    const filledSquares = squares.filter(square => (square === 'X' || square === 'O'))
-    const fillNumber = filledSquares.length
-    const nextFill = fillNumber % 2 === 0 ? 'X' : 'O'
+    const nextFill = getNextFill(squares)
     const newSquares = Array.prototype.slice.call(squares)
     newSquares[index] = nextFill
     setSquares(newSquares)
   }
   return (
     <>
+      <div>{status}</div>
       <div className="board-row">
         <Square value={squares[0]} index={0} handleClick={clickHandler}/>
         <Square value={squares[1]} index={1} handleClick={clickHandler}/>
